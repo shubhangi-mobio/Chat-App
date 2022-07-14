583c8225a4e0e82bfaf610cc90f2d8fad7f31ee3
shubhangi-mobio <shubhangih.mobio@gmail.com>
2022-07-14 16:55:23 +0530

    new changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const path = require('path');
-const http = require('http')
-const express = require('express')
-const socketio = require('socket.io')
-const Filter = require('bad-words')
-const { generateMessage, generateLocationMessage } = require('./utils/messages')
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const Filter = require('bad-words');
+const { generateMessage, generateLocationMessage } = require('./utils/messages');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users');
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 5000
 const publicDirectoryPath = path.join(__dirname, '../public')
